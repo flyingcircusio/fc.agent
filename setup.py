1a2d8f84d72e6ec594e36f5367b7621699688628
Christian Kauhaus <kc@flyingcircus.io>
2014-08-22 17:39:00 +0200

    Preparing release 1.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fc.agent',
-    version='1.3.7.dev0',
+    version='1.4.0',
     author='gocept',
     author_email='mail@gocept.com',
     url='http://bitbucket.org/flyingcircus/fc.agent/',
