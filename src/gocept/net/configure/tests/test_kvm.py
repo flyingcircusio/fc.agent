73b5a77145fcc0515fd50628db716d0c90118eb9
Christian Theune <ct@flyingcircus.io>
2015-06-02 11:28:17 +0200

    Actually, here's the code for updating qemu management. Was accidentally shelved.

--- src/gocept/net/configure/tests/test_kvm.py
+++ src/gocept/net/configure/tests/test_kvm.py
@@ -2,8 +2,6 @@ from gocept.net.configure.kvm import ensure_vms, VM
 import mock
 import os
 import pytest
-import shutil
-import tempfile
 import unittest
 
 
@@ -45,22 +43,6 @@ class KVMConfigTest(unittest.TestCase):
         del os.environ['PUPPET_LOCATION']
         self.p_directory.stop()
 
-    @mock.patch('gocept.net.configure.kvm.VM.ensure_config')
-    def test_no_actions_on_empty_list(self, ensure):
-        with self.assertRaises(SystemExit) as e:
-            ensure_vms()
-        assert e.exception.code == 0
-        self.assertEquals(ensure.call_count, 0)
-
-    @mock.patch('gocept.net.configure.kvm.VM.ensure_config')
-    def test_listed_vms_get_ensure_called(self, ensure):
-        self.fake_directory().list_virtual_machines.return_value = [
-            make_vm()]
-        with self.assertRaises(SystemExit) as e:
-            ensure_vms()
-        assert e.exception.code == 0
-        self.assertEquals(ensure.call_count, 1)
-
     def test_node_deletion(self):
         self.fake_directory().list_virtual_machines.return_value = []
         self.fake_directory().deletions.return_value = {
@@ -88,55 +70,3 @@ class KVMConfigTest(unittest.TestCase):
         assert os.path.exists(cfgs['node01'])
         assert os.path.exists(cfgs['node02'])
         assert not os.path.exists(cfgs['node03'])
-
-
-class VMTest(unittest.TestCase):
-
-    def setUp(self):
-        self.tmpdir = tempfile.mkdtemp()
-        self.p_call = mock.patch('gocept.net.configure.kvm.call')
-        self.call = self.p_call.start()
-
-        self.p_subpcall = mock.patch('subprocess.check_call')
-        self.subpcall = self.p_subpcall.start()
-
-    def tearDown(self):
-        shutil.rmtree(self.tmpdir)
-        self.p_call.stop()
-        self.p_subpcall.stop()
-
-    def test_vm_config_generated_and_ensure_called(self):
-        vm = VM(make_vm(online=True, kvm_host='foo'))
-        vm.ensure_config()
-        assert """\
-name: test00
-parameters:
-  cores: 1
-  disk: 10
-  id: 1000
-  interfaces:
-    srv: {mac: foo}
-  kvm_host: foo
-  memory: 512
-  online: true
-  resource_group: test
-""" == open(vm.configfile, 'r').read()
-
-    def test_vm_old_files_migrated(self):
-        vm = VM(make_vm(online=True, kvm_host='foo'))
-        files = [vm.root + '/run/kvm.test00.pid',
-                 vm.root + '/run/kvm.test00.cfg',
-                 vm.root + '/run/kvm.test00.cfg.in',
-                 vm.root + '/run/kvm.test00.opt',
-                 vm.root + '/run/kvm.test00.opt.in',
-                 vm.root + '/etc/runlevels/default/kvm.test00',
-                 vm.root + '/etc/conf.d/kvm.test00',
-                 vm.root + '/etc/kvm/test00.opt',
-                 vm.root + '/etc/kvm/test00.cfg',
-                 vm.root + '/etc/init.d/kvm.test00']
-        for f in files:
-            open(f, 'w')
-        vm.ensure_config()
-        assert os.path.exists(vm.root + '/run/qemu.test00.pid')
-        for f in files:
-            assert not os.path.exists(f)
