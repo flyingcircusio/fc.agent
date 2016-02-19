e3198d6bbbaf8307753c8267257c9c6bed7b96b6
Christian Zagrodnick <cz@flyingcircus.io>
2016-02-19 16:24:20 +0100

    Back to development: 1.8.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fc.agent',
-    version='1.8.2',
+    version='1.8.3.dev0',
     author='Flying Circus',
     author_email='mail@flyingcircus.io',
     url='http://bitbucket.org/flyingcircus/fc.agent',
