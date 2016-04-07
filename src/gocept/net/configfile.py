6a727a8469bcd0b9ac9957e642040ee467f8479b
Christian Kauhaus <kc@flyingcircus.io>
2016-04-07 11:29:05 +0200

    Replace mounted box directories with symlinks to autofs
    localconfig-box-mounts used to create the mount points directly in the users'
    homes. We now use autofs. The improved localconfig-box-mounts script umounts
    these directories and creates symlink into /mnt/autofs/box. Transition should be
    handled correctly.
    
    Re #19668

--- src/gocept/net/configfile.py
+++ src/gocept/net/configfile.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2011 gocept gmbh & co. kg
-# See also LICENSE.txt
-
 import cStringIO
 import difflib
 import fcntl
--- src/gocept/net/configure/box.py
+++ src/gocept/net/configure/box.py
@@ -1,9 +1,9 @@
 from __future__ import print_function
+from gocept.net.utils import print
 from gocept.net.configfile import ConfigFile
 from gocept.net.directory import Directory, exceptions_screened
-from gocept.net.utils import print
 import os
-import os.path
+import os.path as p
 import subprocess
 
 
@@ -19,31 +19,25 @@ class Exports(object):
             self.users = self.d.list_users()
         self.users = [u for u in self.users if u['class'] == 'human']
         self.create_boxes()
-        # Boxes are not automatically deleted any longer if a user goes away.
-        # The 24h delete cycle will remove the contents anyway but
-        # we now avoid to immediately delete all user information just in case
-        # that the user is not listed any longer. See #13278
 
     def create_boxes(self):
         for u in self.users:
-            box = os.path.join(self.base, u['uid'])
-            if not os.path.exists(box):
+            box = p.join(self.base, u['uid'])
+            if not p.exists(box):
                 print("Creating {}".format(box))
                 os.mkdir(box)
             os.chown(box, u['id'], u['gid'])
-            os.chmod(box, 0755)
+            os.chmod(box, 0o755)
 
 
 class Mounts(object):
 
-    mount_template = (
-        '{server}:/srv/nfs/box/{uid} {home}/box nfs intr,soft,bg,rsize=8192,wsize=8192 0 0\n')
-
-    def __init__(self):
+    def __init__(self, box_server, resource_group):
+        self.box_server = box_server
+        self.resource_group = resource_group
         self.d = Directory()
 
     def __call__(self):
-        self.box_server = os.environ.get('BOX_SERVER')
         if not self.box_server:
             print('No box server. Not configuring.')
             return
@@ -52,41 +46,50 @@ class Mounts(object):
         self.users = [u for u in self.users if u['class'] == 'human']
         self.users.sort(key=lambda u: u['uid'])
 
-        self.ensure_mountpoints()
-        self.ensure_fstab()
-        self.ensure_mounts()
+        self.ensure_symlinks()
+        changed = self.ensure_automap()
+        if changed:
+            subprocess.check_call(['/etc/init.d/autofs', 'restart'])
 
-    def ensure_mountpoints(self):
-        for user in self.users:
-            box = os.path.join(user['home_directory'], 'box')
+    def ensure_symlink(self, user, box):
+        target = p.join('/mnt/autofs/box', user['uid'])
+        if p.islink(box) and os.readlink(box) == target:
+            return
+        if p.ismount(box):
+            print('Box {} is still mounted, unmounting'.format(box))
+            subprocess.check_call(['umount', box])
+        if p.isdir(box):
+            os.rmdir(box)
+        else:
             try:
-                os.lstat(box)
-            except OSError, e:
-                if e.errno == 2:
-                    print("Creating {}".format(box))
-                    os.mkdir(box)
-                elif e.errno == 116:
-                    print("Broken NFS mount. Suspecting a remount will help.")
-                    subprocess.check_call(['umount', box])
-            os.chown(box, user['id'], user['gid'])
+                os.unlink(box)
+            except Exception:
+                pass
+        if not p.exists(box):
+            print('Symlinking {}'.format(box))
+            os.symlink(target, box)
 
-    def ensure_fstab(self):
-        fstab = ConfigFile('/etc/fstab.box')
+    def ensure_symlinks(self):
+        for user in self.users:
+            box = p.join(user['home_directory'], 'box')
+            self.ensure_symlink(user, box)
+
+    autofs_template = (
+        '{uid} -intr,soft,rsize=8192,wsize=8192 {server}:/srv/nfs/box/{uid}\n')
+
+    def ensure_automap(self):
+        fstab = ConfigFile('/etc/autofs/auto.box')
         fstab.write('# Managed by localconfig-box-mounts. '
                     'Manual changes will be overwritten.\n')
         for user in self.users:
-            fstab.write(self.mount_template.format(
+            fstab.write(self.autofs_template.format(
                 server=self.box_server,
-                uid=user['uid'],
-                home=user['home_directory']))
-        fstab.commit()
-
-    def ensure_mounts(self):
-        subprocess.check_call(['mount', '-T', '/etc/fstab.box', '-a'])
+                uid=user['uid']))
+        return fstab.commit()
 
 
 def mounts():
-    m = Mounts()
+    m = Mounts(os.environ.get('BOX_SERVER'), os.environ.get('RESOURCE_GROUP'))
     m()
 
 
