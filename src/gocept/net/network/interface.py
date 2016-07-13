f726f2e8ab7849103c0dac805f7863b9a683a942
Christian Kauhaus <kc@flyingcircus.io>
2016-07-13 17:07:04 +0200

    Gentoo network NG: physical host, standard case
    I've added fixtures for FE only...
    
    Re #20617

--- src/gocept/net/network/interface.py
+++ src/gocept/net/network/interface.py
@@ -1,18 +1,11 @@
 from six import u
+from .annotations import Annotations
 import collections
+import itertools
 import configobj
 import ipaddress
 import os.path as p
 import yaml
-
-
-DEFAULT_ANNOTATIONS = {
-    'mtu': 1500,
-    'defaultroute': False,
-    'metric': 1000,
-    'skip': False,
-    'style': 'managed'
-}
 
 
 class HostConfiguration(object):
@@ -21,30 +14,46 @@ class HostConfiguration(object):
 
     interfaces = None
     interface_by_mac = None
+    conf_header = "# Managed by configure-interface. Don't edit.\n\n"
 
     def __init__(self, enc_yaml, annotations_dir):
         self.enc = yaml.safe_load(enc_yaml) or {}
-        self.enc_interfaces = self.enc.get(
-            'parameters', {}).get('interfaces', {})
+        self.enc_interfaces = self.enc.get('interfaces', {})
         self.annotations_dir = annotations_dir
         self.rt_tables = self.parse_rt_tables()
 
-    def parse_interfaces(self):
+    def create_all_interfaces(self):
         self.interfaces = []
         self.interface_by_mac = collections.defaultdict(list)
         for vlan in self.enc_interfaces.keys():
-            iface = LeafInterface(
-                vlan, self.rt_tables[vlan],
-                self.enc['parameters']['interfaces'][vlan],
-                p.join(self.annotations_dir, vlan + '.cfg'))
-            self.interfaces.append(iface)
-            self.interface_by_mac[iface.mac].append(iface)
+            for iface in self.create_interfaces(vlan):
+                self.interfaces.append(iface)
+                self.interface_by_mac[iface.mac].append(iface)
+
+    def create_interfaces(self, vlan):
+        annotations = Annotations(self.annotations_dir, vlan)
+        if annotations.skip:
+            return []
+        vlan_id = self.rt_tables[vlan]
+        params = self.enc_interfaces[vlan]
+        if params['bridged'] is True or params['bridged'] == 'true':
+            eth = BridgedEth(vlan, vlan_id, params, annotations)
+            br = Bridge(vlan, vlan_id, params, annotations)
+            eth.add_child(br)
+            return [eth, br]
+        else:
+            return [LeafInterface(vlan, vlan_id, params, annotations)]
 
     def confd(self, basedir):
-        res = {}
+        res = collections.defaultdict(list)
         for iface in self.interfaces:
-            res[p.join(basedir, 'iface.' + iface.vlan)] = iface.confd()
-        return res
+            c = iface.confd()
+            if c:
+                res[p.join(basedir, 'iface.' + iface.vlan)].append(c)
+        conffiles = {}
+        for path, snippets in res.items():
+            conffiles[path] = self.conf_header + '\n'.join(snippets)
+        return conffiles
 
     def parse_rt_tables(self):
         rt = {}
@@ -58,24 +67,26 @@ class HostConfiguration(object):
 
 
 class Interface(object):
+
     name = None
     mgm = None
+    parent = None
 
-# XXX __new__ factory
-
-    def __init__(self, vlan, vlan_id, enc, annotations_cfg=None):
+    def __init__(self, vlan, vlan_id, enc, annotations):
         self.vlan = vlan
         self.vlan_id = vlan_id
         self.enc = enc
-        if annotations_cfg and p.exists(annotations_cfg):
-            self.ann = configobj.ConfigObj(annotations_cfg)['interface']
-        else:
-            ann = configobj.ConfigObj()
-            ann['interface'] = DEFAULT_ANNOTATIONS
-            self.ann = ann['interface']
-        if self.ann['style'] == 'managed':
+        self.ann = annotations
+        if self.ann.style == 'managed':
             self.mgm = Static(self)
-        self.dependent = {}
+        self.addrs = {}
+        for net, addrs in sorted(self.enc['networks'].items()):
+            n = ipaddress.ip_network(u(net))
+            if n not in self.addrs:
+                self.addrs[n] = []
+            for a in addrs:
+                self.addrs[n].append(
+                    ipaddress.ip_interface(u'{}/{}'.format(a, n.prefixlen)))
 
     @property
     def name(self):
@@ -85,38 +96,42 @@ class Interface(object):
     def mac(self):
         return self.enc['mac'].upper()
 
-    def interface_addresses(self):
-        for net, addrs in sorted(self.enc['networks'].items()):
-            n = ipaddress.ip_network(u(net))
-            for a in addrs:
-                yield ipaddress.ip_interface(u'{}/{}'.format(a, n.prefixlen))
+    @property
+    def all_addrs(self):
+        return itertools.chain.from_iterable(self.addrs.values())
 
     @property
-    def addresses(self):
-        for addr in self.interface_addresses():
+    def config(self):
+        for addr in self.all_addrs:
             yield str(addr)
 
     @property
     def routes(self):
-        for net, addrs in sorted(self.enc['networks'].items()):
+        for net in sorted(self.addrs.keys(), key=str):
+            if not any(a for a in self.all_addrs
+                       if a.version == net.version):
+                continue
             yield '{} table {}'.format(net, self.vlan_id)
-            if self.ann.as_bool('defaultroute') and addrs != []:
-                gw = self.enc['gateways'].get(net)
+            if self.ann.defaultroute and self.addrs[net]:
+                gw = self.enc['gateways'].get(str(net))
                 if gw:
                     yield 'default via {} table {}'.format(gw, self.vlan_id)
                     yield 'default via {}'.format(gw)
 
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+
     def rules(self, af):
-        for addr in self.interface_addresses():
+        for addr in self.all_addrs:
             if not addr.version == af:
                 continue
             yield 'from {} table {} priority {}'.format(
                 addr, self.vlan_id, self.vlan_id * 10)
-        for net in sorted(self.enc['networks'].keys()):
-            if ipaddress.ip_network(u(net)).version != af:
-                continue
-            yield 'to {} table {} priority {}'.format(
-                net, self.vlan_id, self.vlan_id * 10)
+        for net in sorted(self.addrs.keys(), key=str):
+            if net.version == af:
+                yield 'to {} table {} priority {}'.format(
+                    net, self.vlan_id, self.vlan_id * 10)
 
     @property
     def rules4(self):
@@ -128,11 +143,11 @@ class Interface(object):
 
     @property
     def mtu(self):
-        return self.ann.as_int('mtu')
+        return self.ann.mtu
 
     @property
     def metric(self):
-        return self.ann.as_int('metric')
+        return self.ann.metric
 
     def confd(self):
         raise NotImplementedError()
@@ -141,11 +156,48 @@ class Interface(object):
 class LeafInterface(Interface):
 
     def confd(self):
-        return """\
-# Managed by configure-interface. Don't edit.
+        return self.mgm.confd()
 
+
+class BridgedEth(Interface):
+
+    def __init__(self, vlan, vlan_id, enc, annotations_cfg=None):
+        super(BridgedEth, self).__init__(vlan, vlan_id, enc, annotations_cfg)
+        self.children = []
+
+    def confd(self):
+        return """\
+config_{name}="null"
+mtu_{name}={mtu}
+""".format(name=self.name, mtu=self.mtu)
+
+
+class Bridge(LeafInterface):
+
+    @property
+    def name(self):
+        return 'br' + self.vlan
+
+    def confd(self):
+        return ("""\
+bridge_{name}="{eth}"
+rc_net_{name}_need="net.{eth}"
+""".format(name=self.name, eth=self.parent.name) + '\n' +
+            self.mgm.confd())
+
+
+class MgmStrategy(object):
+
+    def __init__(self, iface):
+        self.iface = iface
+
+
+class Static(MgmStrategy):
+
+    def confd(self):
+        return """\
 config_{name}="\\
-    {addresses}
+    {config}
 "
 
 routes_{name}="\\
@@ -162,39 +214,14 @@ rules6_{name}="\\
 
 mtu_{name}={mtu}
 metric_{name}={metric}
-""".format(addresses='\n    '.join(self.addresses),
-           routes='\n    '.join(self.routes),
-           rules4='\n    '.join(self.rules4),
-           rules6='\n    '.join(self.rules6),
-           mtu=self.mtu, metric=self.metric, name=self.name)
+""".format(config='\n    '.join(self.iface.config),
+           routes='\n    '.join(self.iface.routes),
+           rules4='\n    '.join(self.iface.rules4),
+           rules6='\n    '.join(self.iface.rules6),
+           mtu=self.iface.mtu, metric=self.iface.metric, name=self.iface.name)
 
 
-class VLANMultiplexer(Interface):
-    pass
-
-
-class BridgedEth(Interface):
-    pass
-
-
-class Bridge(LeafInterface):
-
-    @property
-    def name(self):
-        return 'br' + self.name
-
-
-class MgmStrategy(object):
-
-    def __init__(self, iface):
-        self.iface = iface
-
-
-class Static(MgmStrategy):
-    pass
-
-
-class Dynamic(MgmStrategy):
+class DHCP(MgmStrategy):
     pass
 
 
--- src/gocept/net/network/tests/interface_test.py
+++ src/gocept/net/network/tests/interface_test.py
@@ -1,5 +1,6 @@
 from gocept.net.network.interface import HostConfiguration, LeafInterface
-import ipaddress
+from gocept.net.network.annotations import Annotations
+from ipaddress import IPv4Interface, IPv6Interface, IPv4Network, IPv6Network
 import pkg_resources
 import pytest
 import StringIO
@@ -11,34 +12,8 @@ HostConfiguration.RT_TABLES = pkg_resources.resource_filename(
 
 def test_no_interfaces():
     hc = HostConfiguration(StringIO.StringIO(), '')
-    hc.parse_interfaces()
+    hc.create_all_interfaces()
     assert len(hc.interfaces) == 0
-
-
-def test_fc00_confd():
-    hc = HostConfiguration(
-        pkg_resources.resource_stream(__name__, 'fixture/fc00/enc.yaml'),
-        pkg_resources.resource_filename(__name__, 'fixture/fc00/ann'))
-    hc.parse_interfaces()
-    confd = hc.confd('net.d')
-    assert len(confd.keys()) == 2
-    assert confd['net.d/iface.fe'] == str(pkg_resources.resource_string(
-        __name__, 'result/fc00/conf.d/iface.fe'))
-    assert confd['net.d/iface.srv'] == str(pkg_resources.resource_string(
-        __name__, 'result/fc00/conf.d/iface.srv'))
-
-
-def test_interface_addresses():
-    i = LeafInterface('srv', 3, {'networks': {
-        '172.22.48.0/20': ['172.22.48.127'],
-        '2a02:248:101:63::/64':
-        ['2a02:248:101:63::10d6', '2a02:248:101:63::10da'],
-    }})
-    assert set(i.interface_addresses()) == set([
-        ipaddress.IPv4Interface(u'172.22.48.127/20'),
-        ipaddress.IPv6Interface(u'2a02:248:101:63::10d6/64'),
-        ipaddress.IPv6Interface(u'2a02:248:101:63::10da/64'),
-    ])
 
 
 def test_rt_tables():
@@ -46,3 +21,61 @@ def test_rt_tables():
     assert hc.parse_rt_tables() == dict(
         unspec=0, mgm=1, fe=2, srv=3, sto=4, stb=8, dhp=19, default=253,
         main=254, local=255)
+
+
+def test_addrs():
+    i = LeafInterface('srv', 3, {'networks': {
+        '172.22.48.0/20': ['172.22.48.127'],
+        '195.62.126.128/25': [],
+        '2a02:248:101:63::/64':
+        ['2a02:248:101:63::10d6', '2a02:248:101:63::10da'],
+    }}, Annotations())
+    assert i.addrs == {
+        IPv4Network(u'172.22.48.0/20'): [IPv4Interface(u'172.22.48.127/20')],
+        IPv4Network(u'195.62.126.128/25'): [],
+        IPv6Network(u'2a02:248:101:63::/64'): [
+            IPv6Interface(u'2a02:248:101:63::10d6/64'),
+            IPv6Interface(u'2a02:248:101:63::10da/64'),
+        ]
+    }
+
+
+def test_all_addrs():
+    i = LeafInterface('srv', 3, {'networks': {
+        '172.22.48.0/20': ['172.22.48.127'],
+        '2a02:248:101:63::/64':
+        ['2a02:248:101:63::10d6', '2a02:248:101:63::10da'],
+    }}, Annotations())
+    assert set(i.all_addrs) == set([
+        IPv4Interface(u'172.22.48.127/20'),
+        IPv6Interface(u'2a02:248:101:63::10d6/64'),
+        IPv6Interface(u'2a02:248:101:63::10da/64'),
+    ])
+
+# XXX split into test_all_interfaces and individual add_interface tests
+
+
+def test_fc00_confd():
+    hc = HostConfiguration(
+        pkg_resources.resource_stream(__name__, 'fixture/fc00/enc.yaml'),
+        pkg_resources.resource_filename(__name__, 'fixture/fc00/ann'))
+    hc.create_all_interfaces()
+    confd = hc.confd('net.d')
+    assert len(confd.keys()) == 2
+    for vlan in ['fe', 'srv']:
+        assert confd['net.d/iface.{}'.format(vlan)] == str(
+            pkg_resources.resource_string(
+                __name__, 'result/fc00/conf.d/iface.{}'.format(vlan)))
+
+
+def test_lenny_confd():
+    hc = HostConfiguration(
+        pkg_resources.resource_stream(__name__, 'fixture/lenny/enc.yaml'),
+        pkg_resources.resource_filename(__name__, 'fixture/lenny/ann'))
+    hc.create_all_interfaces()
+    confd = hc.confd('net.d')
+    assert len(confd.keys()) == 5  # XXX should be 4 (w/o dhp)
+    for vlan in ['fe', 'srv', 'sto', 'stb']:
+        assert confd['net.d/iface.{}'.format(vlan)] == str(
+            pkg_resources.resource_string(
+                __name__, 'result/lenny/conf.d/iface.{}'.format(vlan)))
