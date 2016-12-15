8eebbc7fe2af5c31e1716ba20d8f7afad4ae978e
Christian Kauhaus <kc@flyingcircus.io>
2016-12-15 15:30:40 +0100

    Preparing release 1.9.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fc.agent',
-    version='1.9.8.dev0',
+    version='1.9.8',
     author='Flying Circus',
     author_email='mail@flyingcircus.io',
     url='http://bitbucket.org/flyingcircus/fc.agent',
