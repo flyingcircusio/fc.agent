84fc454efe84148880c4c464de0d9b6306ed11ef
Christian Kauhaus <kc@flyingcircus.io>
2017-03-09 09:00:33 +0100

    Back to development: 1.9.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fc.agent',
-    version='1.9.10',
+    version='1.9.11.dev0',
     author='Flying Circus',
     author_email='mail@flyingcircus.io',
     url='http://bitbucket.org/flyingcircus/fc.agent',
