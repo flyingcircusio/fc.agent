07fe26ac46452a05b1b5b5a2e0810267205b0973
Christian Theune <ct@flyingcircus.io>
2020-06-16 11:13:47 +0200

    Preparing release 1.10.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fc.agent',
-    version='1.10.12.dev0',
+    version='1.10.12',
     author='Flying Circus',
     author_email='mail@flyingcircus.io',
     url='http://github.com/flyingcircusio/fc.agent',
